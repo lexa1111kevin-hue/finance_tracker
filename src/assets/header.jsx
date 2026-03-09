03295a693bdbcb2a8c40eb92fc212560d120f002
lexa1111kevin-hue <lexa1111kevin@gmail.com>
2026-03-09 22:49:45 +0500

    header ux

--- src/assets/header.jsx
+++ src/assets/header.jsx
@@ -3,9 +3,14 @@ import logo from "../img/logo.png";
 export default function Header() {
   return (
     <header class="sticky top-0 h-auto bg-slate-900 ">
-      <div className="container mx-auto px-4 py-2">
+      <div className="container  items-center flex justify-between mx-auto px-4 py-2">
         <img src={logo} alt="logo" className="w-40  m-0 p-0" />
+
+        <button className="bg-white hover:bg-gray-200 h-full px-4 py-2 m-0 rounded-lg transition">
+          Sign In
+        </button>
       </div>
+      <div></div>
     </header>
   );
 }
